d03c6c181620ffc44b0a113962c0c545b55c9679
Aiden-Coding <dwx_job@163.com>
2024-07-11 20:59:16 +0800

    1

---
+++ docs/components/MyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const message = ref<string>("Hello from MyComponent using setup and TypeScript!");
+</script>
+
+<style>
+/* 你的样式 */
+</style>
--- docs/java/index.md
+++ docs/java/index.md
@@ -1,1 +1,10 @@
 # sd
+# Hello VitePress
+
+这是一个示例文档。
+
+<MyComponent />
+
+<script setup>
+import MyComponent from '../components/MyComponent.vue'
+</script>
